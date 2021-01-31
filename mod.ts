a670cc6f8e31f1bd24726ca6fc568658efd0d2e6
thinceller <thinceller@gmail.com>
2021-01-31 23:01:04 +0900

    Add open function

---
+++ mod.ts
@@ -0,0 +1,14 @@
+import { format } from "https://deno.land/std@0.85.0/datetime/mod.ts";
+
+export async function open(projectName: string, title: string) {
+  const date = format(new Date(), "yyyy/M/d");
+  const url = `https://scrapbox.io/${projectName}/${date} ${title}`;
+
+  const programAliases = {
+    windows: "explorer",
+    darwin: "open",
+    linux: "sensible-browser",
+  };
+  const process = Deno.run({ cmd: [programAliases[Deno.build.os], url] });
+  await process.status();
+}
