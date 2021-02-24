de89245a1b72e11bf94e7983b690565d4104974c
thinceller <thinceller@gmail.com>
2021-02-24 10:52:38 +0900

    Fix url encode

--- mod.ts
+++ mod.ts
@@ -2,13 +2,14 @@ import { format } from "https://deno.land/std@0.85.0/datetime/mod.ts";
 
 export async function open(projectName: string, title: string) {
   const date = format(new Date(), "yyyy/M/d");
-  const url = `https://scrapbox.io/${projectName}/${date} ${title}`;
+  const encodedPath = encodeURIComponent(`${projectName}/${date} ${title}`)
+  const url = `https://scrapbox.io/${encodedPath}`;
 
   const programAliases = {
     windows: "explorer",
     darwin: "open",
     linux: "sensible-browser",
   };
-  const process = Deno.run({ cmd: [programAliases[Deno.build.os], encodeURIComponent(url)] });
+  const process = Deno.run({ cmd: [programAliases[Deno.build.os], url] });
   await process.status();
 }
